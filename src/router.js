961f5793c8226c6c873eb869c7564fea0a48f169
lindunwei <2530163120@qq.com>
2019-06-27 15:31:50 +0800

    构建项目与准备工作

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router =  new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path:'/',
+      name: 'Index',
+      component: ()=> import('./views/index.vue')
+    },
+    {
+      path:'/login',
+      name: 'Login',
+      component: ()=> import('./views/login.vue')
+    }
+  ]
+})
+//路由守卫
+router.beforeEach((to,from,next)=>{
+  //获取登录状态，ele-login自由命名
+  const isLogin = localStorage.ele_login ? true : false;
+  //判断访问的页面，如果是登录页面就正常跳转
+  if(to.path == '/login'){
+    next();
+  }else{
+    //如果访问的是其他路径，则判断登录状态再决定访问路径
+    isLogin ? next() : next('/login');
+  }
+})
+
+
+
+export default router;
---
+++ src/views/login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="login">
+        <div class="logo">
+            <img src="../assets/logo.jpg" alt="my logo">
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style scoped>
+    .login{
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+        background: #fff;
+        box-sizing: border-box;
+    }
+    .login .logo{
+        text-align: center;
+
+    }
+    .login .logo img{
+        width: 150px;
+    }
+</style>
