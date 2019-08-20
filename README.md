08ab228c1a1a250a0f4bedc9cb050c811236fa5e
lindunwei <2530163120@qq.com>
2019-08-20 22:14:15 +0800

    新增收获地址页，表单组件封装和多组件传值

--- README.md
+++ README.md
@@ -32,6 +32,8 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 
 
-## 内容总结 2019/8/18   
+## 内容总结 2019/8/20   
 
-> 更新内容：me.vue页面
+> 更新内容：更新了新增收货地址页面 **addAddress**，这里封装了表单组件 **formBlock**，和标签组件 **TabTag**，难点是三个组件的传值，属于一般的父子组件传值，不难却复杂，往后要优化为非父子组件传值。
+>
+> 笔记待续。。。
--- src/views/Oeders/AddAddress.vue
+++ src/views/Oeders/AddAddress.vue
@@ -2,22 +2,53 @@
     <div class="addAddress">
         <Header :isLeft="true" :title="title" />
 
-        <div class="viewbody"></div>
+        <div class="viewbody">
+            <div class="content">
+                <FormBlock label="联系人" placeholder="姓名" :tags="sexes" @checkSex="checkSex" :sex="addressInfo.sex"/>
+                <FormBlock label="电话" placeholder="手机号码" />
+                <FormBlock label="地址" placeholder="小区/写字楼/学校等" icon="angle-right" />
+                <FormBlock label="门牌号" placeholder="101号楼4单元404" icon="edit" textarea="textarea" />
+                <div class="formblock">
+                    <div class="label-wrap">标签</div>
+                    <TabTag :tags="tags" @checkTag="checkTag" :selectTag="addressInfo.tag"/>
+                </div>
+            </div>
+        </div>
     </div>
 
 </template>
 
 <script>
 import Header from '../../components/Header'
+import FormBlock from '../../components/Orders/FormBlock'
+import TabTag from "../../components/Orders/TabTag";
 export default {
     components:{
-        Header
+        Header,
+        FormBlock,
+        TabTag
     },
     data(){
         return{
-            title: '新增收货地址'
+            title: '新增收货地址',
+            tags: ['家','学校','公司'],
+            sexes:["先生","女士"],
+            addressInfo:{
+                tag: "",
+                sex: ""
+            }
         }
     },
+    methods:{
+        checkTag(item){
+            console.log(item);
+            this.addressInfo.tag = item;
+        },
+        checkSex(item){
+            console.log(item);
+            this.addressInfo.sex = item;
+        }
+    }
 }
 </script>
 
@@ -35,4 +66,22 @@ export default {
         overflow: auto;
         background-color: #f5f5f5;
     }
+    .content{
+        padding-left: 10px;
+        background: #fff;
+        font-size: 0.94rem;
+    }
+    .formblock{
+        background: #fff;
+        border-bottom: 1px solid #eee;
+        display: flex;
+    }
+    .formblock .label-wrap{
+        flex-basis: 17.33333vw;
+        padding: 3.733333vw 0;
+        line-height: 4.8vw;
+        color: #333; 
+        font-weight: 700;
+    }
+
 </style>
